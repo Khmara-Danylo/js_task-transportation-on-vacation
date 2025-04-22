a3074d782e25b199d49b32a75d6835c17e3aa43d
Khmara-Danylo <khmaradanylo.mate@gmail.com>
2025-04-22 20:55:33 +0300

    name correction added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const countDays = days * 40;
+  const dayRentalCost = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  const countDays = days * dayRentalCost;
 
   if (days < 3) {
     return countDays;
   }
 
   if (days >= 3 && days < 7) {
-    return countDays - 20;
+    return countDays - smallDiscount;
   }
 
   if (days >= 7) {
-    return countDays - 50;
+    return countDays - bigDiscount;
   }
 }
 
